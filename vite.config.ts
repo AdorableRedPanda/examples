042dabacac2c604fc68dab7f5059664fba4b507b
gotin <gotin@tecforce.ru>
2022-12-12 22:17:28 +0400

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '',
+    root: './src',
+    server: {
+        open: true,
+        port: 8080,
+    },
+    build: {
+        outDir: '../dist',
+        assetsDir: './',
+        minify: "terser",
+    },
+});
